abdba7bff607807706e87cf6edd37371418f3741
TTOzzi <devttozzi@gmail.com>
2020-03-24 21:50:32 +0900

    Feat: SignUpView 생성
    ViewController 의 view 를 SignUpView 로 설정
    View 내의 textField 와 label 을 바인딩해줌

---
+++ iOS/SignUp/SignUp/View/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  SignUp
+//
+//  Created by TTOzzi on 2020/03/24.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+    @IBOutlet weak var idTextField: InputTextField!
+    @IBOutlet weak var idStatusLabel: StatusMessageLabel!
+    @IBOutlet weak var passwordTextField: InputTextField!
+    @IBOutlet weak var passwordStatusLabel: StatusMessageLabel!
+    @IBOutlet weak var passwordConfirmTextField: InputTextField!
+    @IBOutlet weak var passwordConfirmStatusLabel: StatusMessageLabel!
+    @IBOutlet weak var nameTextField: InputTextField!
+    @IBOutlet weak var nameStatusLabel: StatusMessageLabel!
+}
