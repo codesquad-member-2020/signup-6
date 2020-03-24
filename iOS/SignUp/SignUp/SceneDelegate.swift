65c6596d79e42e3bf306f10ff0a8f8dc406aee88
TTOzzi <devttozzi@gmail.com>
2020-03-24 10:30:01 +0900

    Feat: 프로젝트 생성

---
+++ iOS/SignUp/SignUp/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  SignUp
+//
+//  Created by TTOzzi on 2020/03/24.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
---
+++ iOS/SignUp/SignUp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by TTOzzi on 2020/03/24.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
