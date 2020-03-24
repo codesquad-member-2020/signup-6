a048510b62ba2a4d2b58486811065fd61e01938a
TTOzzi <devttozzi@gmail.com>
2020-03-24 12:14:20 +0900

    Feat: TextFieldLabel 커스텀 클래스로 분리
    폰트 사이즈와 스타일 설정

---
+++ iOS/SignUp/SignUp/View/TextFieldLabel.swift
@@ -0,0 +1,27 @@
+//
+//  TextFieldLabel.swift
+//  SignUp
+//
+//  Created by TTOzzi on 2020/03/24.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldLabel: UILabel {
+    private var textSize: CGFloat = 15.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setProperties()
+    }
+    
+    private func setProperties() {
+        self.font = .systemFont(ofSize: textSize, weight: .semibold)
+    }
+}
