db72f1b071f88fac6f0225ef72a5e62b8e7a636f
TTOzzi <devttozzi@gmail.com>
2020-03-26 00:27:51 +0900

    Feat: ID ViewModel 구현
    id 에 변동이 생기면 정규표현식으로 유효성 검사 후 결과를 isIdValid 에 담아줌
    isIdValid 에 변동이 생기면 idDidChanged 클로저를 실행

---
+++ iOS/SignUp/SignUp/ViewModel/IdViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  IdViewModel.swift
+//  SignUp
+//
+//  Created by TTOzzi on 2020/03/25.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class IdViewModel {
+    private let idRegExr = "^[a-z0-9_-]{5,20}$"
+    var id = Dynamic<String>.init("")
+    var idDidChanged: ((Bool) -> Void) = { _ in }
+    private var isIdValid = false {
+        didSet {
+            idDidChanged(isIdValid)
+        }
+    }
+    
+    init() {
+        id.bind = { text in
+            self.isIdValid = self.verifyIdInput(id: text)
+        }
+    }
+    
+    func verifyIdInput(id: String) -> Bool {
+        return !id.match(with: idRegExr).isEmpty
+    }
+}
