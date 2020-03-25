96d08d3cd7c16d62cde9fb2333fc7a4f34c9265a
TTOzzi <devttozzi@gmail.com>
2020-03-25 11:14:41 +0900

    Test: 테스트 코드 수정
    하나의 테스트 메소드가 하나만 테스트하도록 수정
    함수 구조를 바꿔서 더이상 반환값이 없어 테스트가 불가능하지만 일단 수정했음

--- iOS/SignUp/InputVerifierTests/InputVerifierTests.swift
+++ iOS/SignUp/InputVerifierTests/InputVerifierTests.swift
@@ -15,27 +15,47 @@ class InputVerifierTests: XCTestCase {
         verifier = InputVerifier()
     }
 
-    func testIdVerify() {
+    func testLessThanFiveChrId() {
         XCTAssertFalse(verifier.verifyIdInput(id: "abc"))
+    }
+    
+    func testMoreThanTwentyChrId() {
         XCTAssertFalse(verifier.verifyIdInput(id: "12345678901234567890abcde"))
+    }
+    
+    func testUnavailableSpecialChrId() {
         XCTAssertFalse(verifier.verifyIdInput(id: "abcde.fg"))
+    }
+    
+    func testAvailableId() {
         XCTAssertTrue(verifier.verifyIdInput(id: "a_b_c_d-e1234"))
     }
     
-    func testPasswordVerify() {
-        // 8글자 미만
+    func testLessThanEightChrPW() {
         XCTAssertFalse(verifier.verifyPasswordInput(password: "Db1004@").0)
-        // 16글자 초과
+    }
+    
+    func testMoreThanSixteenChrPW() {
         XCTAssertFalse(verifier.verifyPasswordInput(password: "Db1004@1234567890").0)
-        // 대문자 없음
+    }
+    
+    func testNotIncludedUpperCasePW() {
         XCTAssertFalse(verifier.verifyPasswordInput(password: "db1004@@").0)
-        // 소문자 없음
+    }
+    
+    func testNotIncludedLowerCasePW() {
         XCTAssertFalse(verifier.verifyPasswordInput(password: "DB1004@@").0)
-        // 숫자 없음
+    }
+    
+    func testNotIncludedNumberPW() {
         XCTAssertFalse(verifier.verifyPasswordInput(password: "Dbangelcodesquad").0)
-        // 특수문자 없음
+    }
+    
+    func testNotIncludedSpecialChrPW() {
         XCTAssertFalse(verifier.verifyPasswordInput(password: "Db1004codesquad").0)
-        // 모든 조건 만족
+    }
+    
+    func testAvailablePW() {
         XCTAssertTrue(verifier.verifyPasswordInput(password: "Db1004@@@squid").0)
     }
 }
