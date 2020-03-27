2571aed979e5d15287ca4ccf32513d09fec02c1b
Ever <hsik0225@gmail.com>
2020-03-27 13:42:13 +0900

    Feat : 사용자 개인정보 조회

--- BE/src/main/java/com/codesquad/signup/api/ApiUserController.java
+++ BE/src/main/java/com/codesquad/signup/api/ApiUserController.java
@@ -5,13 +5,14 @@ import com.codesquad.signup.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,7 +29,13 @@ public class ApiUserController {
     }
 
     @PostMapping("")
-    public User create(@RequestBody User user) {
-        return userRepository.save(user);
+    public String create(@RequestBody User user) {
+        userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public User read(@PathVariable BigInteger id) {
+        return userRepository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
 }
