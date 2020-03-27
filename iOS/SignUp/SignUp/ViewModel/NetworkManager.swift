70ca8bbf17e8786271073e75c8efd291829271c2
TTOzzi <devttozzi@gmail.com>
2020-03-27 15:16:21 +0900

    Refactor: 바뀐 서버 양식에 맞게 코드 수정

--- iOS/SignUp/SignUp/ViewModel/NetworkManager.swift
+++ iOS/SignUp/SignUp/ViewModel/NetworkManager.swift
@@ -12,7 +12,7 @@ class NetworkManager {
     private let session: URLSession = URLSession.shared
     private let serverUrl = "https://87cda762-6dbe-4c52-a3cb-297f7f4c88dd.mock.pstmn.io"
     var idConfirmUrl: String {
-        return serverUrl + "/api/users/exist?userId="
+        return serverUrl + "/api/users/exist/userId?userId="
     }
     
     func getRequest(url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
