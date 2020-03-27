d8ca0005f6d5ebc7222fdc8037f4fd0c9b9ee555
Reese Kim <reesekimm@gmail.com>
2020-03-27 23:52:22 +0900

    Feat: 약관 팝업 스크롤링에 따른 버튼 활성화 기능 추가
    - 약관 안내문 영역에서 스크롤을 끝까지 내렸을때
    동의 버튼 활성화
    - 활성화된 동의버튼 클릭 시 체크박스 체크 및 팝업 닫힘

--- FE/src/js/terms.js
+++ FE/src/js/terms.js
@@ -19,17 +19,48 @@ const renderModal = () => {
 };
 
 const bindEventListener = () => {
+	// close modal
 	const closeButton = document.querySelector(".closeBtn");
 	closeButton.addEventListener("click", closeByClickingButton);
 	window.addEventListener("click", closeByClickingOutside);
+
+	// activate agreement button when user scroll down to the bottom
+	const contentArea = document.querySelector(".modal_content");
+	contentArea.addEventListener("scroll", handleButtonActivation);
 };
 
 const closeByClickingButton = () => {
 	modal.classList.add("close");
+	deactivateButton();
 };
 
 const closeByClickingOutside = e => {
 	if (e.target === modal) {
 		modal.classList.add("close");
+		deactivateButton();
 	}
+};
+
+const handleButtonActivation = e => {
+	const content = e.target;
+	const isBottom = content.scrollHeight - content.scrollTop === content.clientHeight;
+	if (isBottom) activateButton();
+};
+
+const activateButton = () => {
+	const agreementButton = modal.querySelector("button");
+	agreementButton.classList.add("active");
+	agreementButton.addEventListener("click", check);
+};
+
+const deactivateButton = () => {
+	const agreementButton = modal.querySelector("button");
+	agreementButton.classList.remove("active");
+	agreementButton.removeEventListener("click", check);
+};
+
+const check = () => {
+	const checkbox = document.getElementById("terms");
+	checkbox.setAttribute("checked", true);
+	modal.classList.add("close");
 };
