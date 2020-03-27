da161cdf7ff513fca46e5bb7b297edfbedfffbb9
TTOzzi <devttozzi@gmail.com>
2020-03-27 14:44:13 +0900

    Refactor: CustomSegue 로직 변경
    애니메이션 삭제
    navigationController 로 push, pop

--- iOS/SignUp/SignUp/View/Segues/PushSegue.swift
+++ iOS/SignUp/SignUp/View/Segues/PushSegue.swift
@@ -10,21 +10,6 @@ import UIKit
 
 class PushSegue: UIStoryboardSegue {
     override func perform() {
-        let src = self.source
-        let dst = self.destination
-        dst.modalPresentationStyle = .fullScreen
-        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
-        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
-
-        UIView.animate(withDuration: 0.5,
-                              delay: 0.0,
-                            options: .curveEaseInOut,
-                         animations: {
-                                dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
-                                },
-                        completion: { finished in
-                                src.present(dst, animated: false, completion: nil)
-                                    }
-                        )
+        source.navigationController?.pushViewController(destination, animated: true)
     }
 }
